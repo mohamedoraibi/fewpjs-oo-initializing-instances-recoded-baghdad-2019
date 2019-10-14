2dbfb9e906458c03506625b8c091cb79d47f6e8f
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-10-14 08:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ this.drink=drink;
 }
 
 class Lunch{
-  
+    constructor(salad,soup,drink){
+this.salad=salad;
+this.soup=soup;
+this.drink=drink;
+  }
 }
 
 
 class Dinner{
-  
+      constructor(salad,soup,drink){
+this.salad=salad;
+this.soup=soup;
+this.drink=drink;
+  }
 }
+
+
+
+3. Create a class for `Dinner`. Since dinner is a little bit fancier, `Dinner` will have a constructor with salad, soup, entree, and dessert. Indicate that dessert is intended to be a private property by including an underscore (`_`) at the beginning.
